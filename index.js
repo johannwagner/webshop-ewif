2d1c76f2eb88b9c83fe1ac947e895e20ea2e391d
Johann Wagner <der.yojo@gmail.com>
2018-01-04 10:51:37 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,124 @@
+
+function showSite(siteName) {
+
+	const allSites = ['overview', 'shoppingcart', 'allbooks']
+
+	allSites.forEach((site) => {
+		const domElement = document.getElementById(site)
+		const menuDomElement = document.getElementById(site + 'MenuItem')
+
+		domElement.style.display = 'none';
+		menuDomElement.className = '';
+	})
+
+	selectedSite = siteName;
+
+	document.getElementById(siteName).style.display = 'flex';
+	document.getElementById(siteName + 'MenuItem').className = 'selectedMenuItem';
+}
+
+function renderBook(bookData, containerId, noAddToCart) {
+	/*
+		bookData.title
+		bookData.author
+		bookData.coverImage
+	*/
+
+	// Load Template
+
+
+	$.ajax({
+    	url : "http://localhost:5000/book.html",
+    	async: false,
+    	success : function(htmlTemplate){
+			// Set Data in Template
+
+    		htmlTemplate = htmlTemplate.replace('{title}', bookData.title);
+    		htmlTemplate = htmlTemplate.replace('{coverImage}', bookData.coverImage);
+    		htmlTemplate = htmlTemplate.replace('{author}', bookData.author);
+    		htmlTemplate = htmlTemplate.replace('{id}', bookData.id);
+    		htmlTemplate = htmlTemplate.replace('{price}', bookData.price);
+
+    		htmlTemplate = htmlTemplate.replace('{cardclass}', noAddToCart ? 'DisplayNone' : '')  
+    		// Render Template
+
+    		if(containerId) {
+    			document.getElementById(containerId).innerHTML += htmlTemplate;
+    		} else {
+    			// No Container Id
+    			document.write(htmlTemplate);
+    		}
+
+        } 
+    });
+
+
+}
+
+function renderHighlightedBooks() {
+	document.getElementById('overviewBookList').innerHTML = null;
+
+	const randomIndex = Math.floor(Math.random() * bookList.length);
+	const randomIndex2 = Math.floor(Math.random() * bookList.length);
+	const randomIndex3 = Math.floor(Math.random() * bookList.length);
+
+	const randomIndexes = [randomIndex, randomIndex2, randomIndex3];
+	const highlightedBooks = randomIndexes.map(function (rIndex) {
+		return bookList[rIndex];
+	})
+
+	for(index in highlightedBooks) {
+		const book = highlightedBooks[index];
+
+		renderBook(book, 'overviewBookList');
+	}
+}
+
+function renderAllBooks() {
+	document.getElementById('allbooksBookList').innerHTML = null;
+
+	for (index in bookList) {
+		const book = bookList[index];
+
+		renderBook(book, 'allbooksBookList');
+	}
+}
+
+function renderShoppingCart() {
+	document.getElementById('shoppingcartBookList').innerHTML = null;
+
+
+	const bookListForShoppingCart = shoppingCart.map((id) => {
+		return bookList[id];
+	})
+
+	for (index in bookListForShoppingCart) {
+		const book = bookListForShoppingCart[index];
+
+		renderBook(book, 'shoppingcartBookList', true);
+	}
+
+
+}
+
+function renderShoppingCartBubble() {
+	document.getElementById('shoppingCartBubble').innerHTML = shoppingCart.length;
+}
+
+function addBookToCart(id) {
+	if(!shoppingCart.includes(id)) {
+		shoppingCart.push(id);
+
+
+		console.log('addedBookId: ', id);
+		console.log('newShoppingCart: ', shoppingCart);
+
+
+		renderShoppingCart();
+		renderShoppingCartBubble();
+
+		showSite('shoppingcart');
+	}
+
+	
+}
